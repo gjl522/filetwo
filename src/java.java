8033da02f569f1a677ff45beba1e78800dcb1341
DELL <986174990@qq.com>
2020-10-22 17:16:27 +0800

    002分支提交啦

--- src/java.java
+++ src/java.java
@@ -4,5 +4,7 @@ public class java {
         System.out.println("这是主分支");
 
         System.out.println("主分支中的001分支,这个才是真的001分支");
+
+        System.out.println("这是又分离的002分支,这个才是真正的002分支");
     }
 }
